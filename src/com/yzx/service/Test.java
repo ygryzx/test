809aec1fca14a5eda658080a1745b1b78792d8af
yangzx <947890795@qq.com>
2018-07-19 11:32:08 +0800

    1.变动

--- src/com/yzx/service/Test.java
+++ src/com/yzx/service/Test.java
@@ -1,5 +1,6 @@
 package com.yzx.service;
 
+import com.yzx.test.Hello;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +10,8 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
 		HelloWorld hello=(HelloWorld) ac.getBean("hello");
+		Hello hello1=(Hello) ac.getBean("hello1");
 		hello.say();
+		hello1.say1();
 	}
 }
---
+++ src/com/yzx/test/Hello.java
@@ -0,0 +1,9 @@
+package com.yzx.test;
+
+public class Hello {
+
+	public void say1() {
+		System.out.println("Hello Spring4!!");
+		System.out.println("我在测试git更新代码！！！");
+	}
+}
